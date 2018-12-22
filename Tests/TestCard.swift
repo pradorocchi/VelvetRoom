711fe0c3ddb430c88ca35210d76daa22360580a7
vauxhall <zero.griffin@gmail.com>
2018-12-22 15:34:29 +0100

    Test update content

--- Tests/TestCard.swift
+++ Tests/TestCard.swift
@@ -18,4 +18,16 @@ class TestCard:XCTestCase {
         XCTAssertTrue(card.positions.isEmpty)
         XCTAssertTrue(card.contents.isEmpty)
     }
+    
+    func testNewContent() {
+        let time = Date().timeIntervalSince1970
+        let card = Card()
+        card.content = "hello world"
+        card.content = "lorem ipsum"
+        XCTAssertEqual(2, card.contents.count)
+        XCTAssertEqual("hello world", card.contents.first!.value)
+        XCTAssertLessThan(time, card.contents.first!.time)
+        XCTAssertEqual("lorem ipsum", card.contents.last!.value)
+        XCTAssertLessThan(card.contents.first!.time, card.contents.last!.time)
+    }
 }
