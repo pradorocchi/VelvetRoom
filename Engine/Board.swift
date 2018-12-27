bce45063f6ca4941e816133e2fb95b51fb09a5c7
vauxhall <zero.griffin@gmail.com>
2018-12-27 13:49:30 +0100

    Add progress to board

--- Engine/Board.swift
+++ Engine/Board.swift
@@ -7,4 +7,12 @@ public class Board:Codable {
     public internal(set) var updated = 0.0
     public internal(set) var columns = [Column]()
     public internal(set) var cards = [Card]()
+    public var progress:Float {
+        guard !cards.isEmpty else { return 0 }
+        return cards.reduce(into:0, { result, card in
+            if card.column == columns.count - 1 {
+                result += 1
+            }
+        }) / Float(cards.count)
+    }
 }
--- Tests/TestBoard.swift
+++ Tests/TestBoard.swift
@@ -26,4 +26,65 @@ class TestBoard:XCTestCase {
         XCTAssertTrue(board.columns.isEmpty)
         XCTAssertTrue(board.cards.isEmpty)
     }
+    
+    func testProgressNoColumns() {
+        let board = Board()
+        XCTAssertEqual(0, board.progress)
+    }
+    
+    func testProgressOneColumnNoCards() {
+        let board = Board()
+        board.columns = [Column()]
+        XCTAssertEqual(0, board.progress)
+    }
+    
+    func testProgressOneColumnWithCards() {
+        let board = Board()
+        let cardA = Card()
+        let cardB = Card()
+        cardA.position(column:0, index:0)
+        cardB.position(column:0, index:1)
+        board.columns = [Column()]
+        board.cards = [cardA, cardB]
+        XCTAssertEqual(1, board.progress)
+    }
+    
+    func testProgressTwoColumnNoCards() {
+        let board = Board()
+        board.columns = [Column(), Column()]
+        XCTAssertEqual(0, board.progress)
+    }
+    
+    func testProgressTwoColumnsWithCards() {
+        let board = Board()
+        let cardA = Card()
+        let cardB = Card()
+        cardA.position(column:0, index:0)
+        cardB.position(column:1, index:0)
+        board.columns = [Column(), Column()]
+        board.cards = [cardA, cardB]
+        XCTAssertEqual(0.5, board.progress)
+    }
+    
+    func testProgressThreeColumnsWithCards() {
+        let board = Board()
+        let cardA = Card()
+        let cardB = Card()
+        cardA.position(column:0, index:0)
+        cardB.position(column:1, index:0)
+        board.columns = [Column(), Column(), Column()]
+        board.cards = [cardA, cardB]
+        XCTAssertEqual(0, board.progress)
+    }
+    
+    func testProgressThreeColumnsWithCardsCompleted() {
+        let board = Board()
+        let cardA = Card()
+        let cardB = Card()
+        cardA.position(column:2, index:0)
+        cardB.position(column:2, index:0)
+        board.columns = [Column(), Column(), Column()]
+        board.cards = [cardA, cardB]
+        XCTAssertEqual(1, board.progress)
+    }
 }
