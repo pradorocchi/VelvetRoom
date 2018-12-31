8053caead16846f06bef484c07c07a32a39725af
vauxhall <zero.griffin@gmail.com>
2018-12-31 15:16:52 +0100

    Add cloud kit

--- Engine/CloudSynch.swift
+++ Engine/CloudSynch.swift
@@ -1,4 +1,5 @@
 import Foundation
+import CloudKit
 
 class CloudSynch:Synch {
     var notification:(([String:TimeInterval]) -> Void)!
@@ -13,22 +14,49 @@ class CloudSynch:Synch {
     }
     
     func load(_ id:String) {
-
+        DispatchQueue.global(qos:.background).async {
+            CKContainer(identifier:"iCloud.VelvetRoom").publicCloudDatabase.fetch(withRecordID:.init(recordName:id))
+            { record, _ in
+                if let json = record?["json"] as? CKAsset,
+                    let data = try? Data(contentsOf:json.fileURL),
+                    let board = try? JSONDecoder().decode(Board.self, from:data) {
+                    self.loaded(board)
+                }
+            }
+        }
     }
     
     func save(_ account:[String:TimeInterval]) {
-
+        if started {
+            DispatchQueue.global(qos:.background).async {
+                NSUbiquitousKeyValueStore.default.set(account, forKey:"velvetroom.boards")
+                NSUbiquitousKeyValueStore.default.synchronize()
+            }
+        }
     }
     
     func save(_ board:Board) {
-
+        if started {
+            DispatchQueue.global(qos:.background).async {
+                let record = CKRecord(recordType:"Board", recordID:.init(recordName:board.id))
+                record["json"] = CKAsset(fileURL:LocalStorage.url(board.id))
+                let operation = CKModifyRecordsOperation(recordsToSave:[record])
+                operation.savePolicy = .allKeys
+                CKContainer(identifier:"iCloud.VelvetRoom").publicCloudDatabase.add(operation)
+            }
+        }
     }
     
     private func register() {
-        
+        NotificationCenter.default.addObserver(forName:NSUbiquitousKeyValueStore.didChangeExternallyNotification,
+                                               object:nil, queue:OperationQueue()) { _ in self.fetch() }
+        started = NSUbiquitousKeyValueStore.default.synchronize()
     }
     
     private func fetch() {
-
+        if let account = NSUbiquitousKeyValueStore.default.dictionary(
+            forKey:"velvetroom.boards") as? [String:TimeInterval] {
+            notification(account)
+        }
     }
 }
