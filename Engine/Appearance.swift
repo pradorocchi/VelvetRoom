3165b729a015d1d20238cf213622d278132cf776
vauxhall <zero.griffin@gmail.com>
2019-01-24 09:54:25 +0100

    Add backwards compatibility to account

---
+++ Engine/Appearance.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public enum Appearance:Int, Codable {
+    case dark
+    case light
+    case system
+}
--- Tests/TestAccount.swift
+++ Tests/TestAccount.swift
@@ -2,22 +2,50 @@ import XCTest
 @testable import VelvetRoom
 
 class TestAccount:XCTestCase {
-    private var data:Data!
-    private let json = """
+    func testParsingOld() {
+        let json = """
 {
 "boards": ["lorem ipsum"],
 "rates": [],
 "rateTries": 1
 }
 """
-    override func setUp() {
-        data = Data(json.utf8)
-    }
-    
-    func testParsing() {
-        let account = try! JSONDecoder().decode(Account.self, from:data)
+        let account = try! JSONDecoder().decode(Account.self, from:Data(json.utf8))
         XCTAssertEqual("lorem ipsum", account.boards.first!)
         XCTAssertTrue(account.rates.isEmpty)
         XCTAssertEqual(1, account.rateTries)
+        XCTAssertEqual(14, account.font)
+        XCTAssertEqual(.system, account.appearance)
+    }
+    
+    func testParsingNew() {
+        let json = """
+{
+"boards": ["lorem ipsum"],
+"rates": [],
+"rateTries": 1,
+"appearance": 1,
+"font": 30
+}
+"""
+        let account = try! JSONDecoder().decode(Account.self, from:Data(json.utf8))
+        XCTAssertEqual("lorem ipsum", account.boards.first!)
+        XCTAssertTrue(account.rates.isEmpty)
+        XCTAssertEqual(1, account.rateTries)
+        XCTAssertEqual(30, account.font)
+        XCTAssertEqual(.light, account.appearance)
+    }
+    
+    func testSerialise() {
+        let json = """
+{"boards":["hello world"],"rates":[-978307200],"rateTries":10,"font":30,"appearance":0}
+"""
+        var account = Account()
+        account.font = 30
+        account.appearance = .dark
+        account.rates = [Date(timeIntervalSince1970:0)]
+        account.rateTries = 10
+        account.boards = ["hello world"]
+        XCTAssertEqual(json, String(decoding:try! JSONEncoder().encode(account), as:UTF8.self))
     }
 }
