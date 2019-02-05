5021badcb709518c58421059743ceeaeb69c2c9e
vauxhall <zero.griffin@gmail.com>
2019-02-05 09:05:09 +0100

    Remove application

--- Mac/View.swift
+++ Mac/View.swift
@@ -1,7 +1,42 @@
 import AppKit
 import VelvetRoom
 
-class View:NSWindow {
+@NSApplicationMain class View:NSWindow, NSApplicationDelegate, NSWindowDelegate {
+    static var shared:Application { return NSApp.delegate as! Application }
+    var skin = Skin() { didSet { Skin.post() } }
+    private(set) weak var view:View!
+//    @IBOutlet private(set) weak var list:NSMenuItem!
+//    @IBOutlet private(set) weak var newColumn:NSMenuItem!
+//    @IBOutlet private(set) weak var newCard:NSMenuItem!
+//    @IBOutlet private(set) weak var find:NSMenuItem!
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_:NSApplication) -> Bool { return true }
+    
+    func applicationDidFinishLaunching(_:Notification) {
+        view = NSApp.windows.first as? View
+        view.delegate = self
+    }
+    
+    func applicationWillTerminate(_:Notification) {
+        view.fireSchedule()
+    }
+    
+    func windowWillBeginSheet(_:Notification) {
+        view.menu!.items.forEach { $0.isEnabled = false }
+    }
+    
+    func windowDidEndSheet(_:Notification) {
+        view.menu!.items.forEach { $0.isEnabled = true }
+    }
+    
+    func window(_:NSWindow, willPositionSheet:NSWindow, using rect:NSRect) -> NSRect {
+        var rect = rect
+        rect.origin.y += 36
+        return rect
+    }
+    
+    
+    
     let repository = Repository()
     let alert = Alert()
     weak var root:ItemView?
@@ -33,6 +68,7 @@ class View:NSWindow {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        UserDefaults.standard.set(false, forKey:"NSFullScreenMenuItemEverywhere")
         makeOutlets()
         repository.list = { boards in DispatchQueue.main.async { self.list(boards) } }
         repository.select = { board in DispatchQueue.main.async { self.select(board) } }
